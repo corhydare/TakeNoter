b05407c2b8fbdca4eb4f5e0eb463187092ffcb05
Unknown <danila.vpopov@gmail.com>
2022-03-05 12:32:52 -0800

    consolidating

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const routing = require("./routes/routes1")(app);
+
+const app = express();
+
+app.get("/routes/api", (req, res) => res.send("yo"));
+
+const port = process.env.PORT || 9000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
