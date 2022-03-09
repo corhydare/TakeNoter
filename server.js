7ecec8738a08d27a6ca1b47d59df1b2d043b96ed
Unknown <danila.vpopov@gmail.com>
2022-03-08 17:18:16 -0800

    port 80?

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const port = process.env.port || 3001;
+const port = process.env.port || 80;
 const app = express();
 const html = require("./routes/htmlRoutes");
 const api = require("./routes/apiRoutes");
